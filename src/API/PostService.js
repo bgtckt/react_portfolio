2aeee50c1ba78cc4ee0c2262301d7096757afd92
Алексей Анпилов <bgtckt@gmail.com>
2022-01-10 16:10:33 +0300

    Changed PostService.js

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -5,11 +5,10 @@ export default class PostService {
 
   // метод лля получения данных о книгах из Google Books
   static async getBooks (query) {
-    const apiKey = 'AIzaSyD8fT_s0YvFbNUIVfrlzXjB34O_GxRftBY';
 
     return await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`, {
       params: {
-        authorization: apiKey,
+        authorization: process.env.REACT_APP_BOOKS_KEY,
         startIndex: 0,
         maxResults: 36
       }
